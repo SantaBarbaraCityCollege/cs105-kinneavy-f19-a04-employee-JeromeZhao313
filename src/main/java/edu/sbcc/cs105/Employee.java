c4cedc2dbbdfd202a8c20a2131a53caaf3efe0b3
Ziyi Zhao <zhaojerome@yahoo.com>
2019-09-17 21:46:39 -0700

    finished

--- src/main/java/edu/sbcc/cs105/Employee.java
+++ src/main/java/edu/sbcc/cs105/Employee.java
@@ -47,6 +47,6 @@ public class Employee {
      */
     public void raiseSalary(double byPercent) {
       this.byPercent = byPercent;
-      currentSalary = currentSalary * (1 + byPercent/100);
+      currentSalary = currentSalary *(1 + byPercent/100);
     }
 }
